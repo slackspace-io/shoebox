9d3c58c0dd414ee1a8670971720124d3c2db41f4
Jacob Morgan <jtuba05@gmail.com>
2024-12-01 18:35:58 +0100

    Working model to store into DB

---
+++ migrations/2024-12-01-164008_media_files/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+CREATE TABLE media_files (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       asset_type TEXT NOT NULL,
+                       path TEXT
+);
