433b095c8c619eb4eadf8803e1d0f708e721d893
Jacob Morgan <jtuba05@gmail.com>
2024-12-01 15:16:49 +0100

    Updated to 7.x restart without diesel

--- src/filesystem/fs_watcher.rs
+++ src/filesystem/fs_watcher.rs
@@ -1,6 +1,8 @@
-use std::fs;
 
-#[derive(Debug)]
+use std::fs;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
 pub enum FileType {
     Video(String),
     Photo(String),
@@ -8,9 +10,7 @@ pub enum FileType {
 }
 
 
-pub fn scan_files(dir: &str) -> Vec<FileType> {
-    #[cfg(feature = "ssr")]
-    use crate::db::db_calls::insert_media_file;
+pub async fn scan_files(dir: &str) -> Vec<FileType> {
     let mut files = Vec::new();
 
     // Iterate over entries in the specified directory
@@ -21,11 +21,9 @@ pub fn scan_files(dir: &str) -> Vec<FileType> {
                 let ext = extension.to_string_lossy().to_lowercase();
                 if matches!(ext.as_str(), "jpg" | "jpeg" | "png" | "gif") {
                     #[cfg(feature = "ssr")]
-                    insert_media_file(path.file_name().unwrap().to_str().unwrap(), path.display().to_string().as_str());
                     files.push(FileType::Photo(path.display().to_string()));
                 } else if matches!(ext.as_str(), "mp4" | "mkv" | "avi" | "mov") {
                     #[cfg(feature = "ssr")]
-                    insert_media_file(path.file_name().unwrap().to_str().unwrap(), path.display().to_string().as_str());
                     files.push(FileType::Video(path.display().to_string()));
                 } else {
                     files.push(FileType::Other(path.display().to_string()));
